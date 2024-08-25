e13fe2e505bdaf1834d91732e880293ef4970507
Jaswin <114131186+Jaswinmohan@users.noreply.github.com>
2024-08-25 13:36:39 +0530

    Time: 1 ms (99.98%), Space: 45.5 MB (71.11%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,11 +1,15 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int targetNumIndex = nums.length - 1;
-        for (int i = nums.length - 2; i >= 0; i--) {
-            if (targetNumIndex <= i + nums[i]) {
-                targetNumIndex = i;
+        int n=nums.length;
+        int dest=n-1;
+        if(nums[0]>=nums.length-1){
+            return true;
+        }for(int i=n-2;i>=0;i--){
+            if(nums[i]+i>=dest){
+                dest=i;
+            }if(dest==0){
+                return true;
             }
-        }
-        return targetNumIndex == 0;
+        }return false;
     }
 }
